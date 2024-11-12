7a36d4db4257e06e7517650b06520f4df8257770
PoQuatre <leflem.mateo2002@gmail.com>
2024-11-12 03:39:15 +0100

    fix: check memset return value

--- tests/ft_memset.c
+++ tests/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 02:49:35 by mle-flem          #+#    #+#             */
-/*   Updated: 2024/11/12 02:54:24 by mle-flem         ###   ########.fr       */
+/*   Updated: 2024/11/12 03:38:31 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,21 @@ int	main(void)
 	char tab[100];
 
 	memset(tab, 0, 100);
-	ft_memset(tab, 'A', 0);
+	char *ret = ft_memset(tab, 'A', 0);
 
-	/* 1 */ check(tab[0] == 0);
+	/* 1 */ check(tab[0] == 0 && ret == tab);
 
 	memset(tab, 0, 100);
-	ft_memset(tab, 'A', 42);
+	ret = ft_memset(tab, 'A', 42);
 	int c = 0;
 	while (c < 100 && tab[c] == 'A') c++;
 
-	/* 2 */ check(c == 42 && tab[42] == 0);
+	/* 3 */ check(c == 42 && tab[42] == 0 && ret == tab);
 
 	memset(tab, 0, 100);
-	ft_memset(tab, 'A' + 256, 42);
+	ret = ft_memset(tab, 'A' + 256, 42);
 	c = 0;
 	while (c < 100 && tab[c] == 'A') c++;
 
-	/* 3 */ check(c == 42 && tab[42] == 0);
+	/* 4 */ check(c == 42 && tab[42] == 0 && ret == tab);
 }
