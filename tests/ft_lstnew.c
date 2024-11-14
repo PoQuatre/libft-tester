977b8ba6fdd3b8806dcff0568a6323e4bb7f8aef
mle-flem <mle-flem@student.42.fr>
2024-11-14 21:51:00 +0100

    feat: add ft_lstnew test cases

--- tests/ft_lstnew.c
+++ tests/ft_lstnew.c
@@ -6,9 +6,12 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 05:31:37 by mle-flem          #+#    #+#             */
-/*   Updated: 2024/11/10 19:23:23 by mle-flem         ###   ########.fr       */
+/*   Updated: 2024/11/14 21:50:46 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <signal.h>
+#include <stdlib.h>
 
 #include "utils.h"
 
@@ -16,6 +19,11 @@
 
 int	main(void)
 {
-	check(ft_lstnew(""));
-	return 0;
+	signal(SIGSEGV, handle_segv);
+
+	t_list *l = ft_lstnew((void *) 42);
+	/* 1 */ check(l->content == (void *) 42);
+	/* 2 */ check(!l->next);
+	/* 3 */ mcheck(l, sizeof(t_list));
+	free(l);
 }
