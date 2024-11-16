c5f80b4d018228aca48543daa8b31aca97de351e
PoQuatre <leflem.mateo2002@gmail.com>
2024-11-16 07:34:54 +0100

    feat(strrchr): check if null byte is compared against an unsigned char

--- tests/ft_strrchr.c
+++ tests/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 21:22:50 by mle-flem          #+#    #+#             */
-/*   Updated: 2024/11/10 21:58:31 by mle-flem         ###   ########.fr       */
+/*   Updated: 2024/11/16 07:34:30 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int	main(void)
 	/* 3 */ check(ft_strrchr(s, 'z') == NULL);
 	/* 4 */ check(ft_strrchr(s, 0) == s + 9);
 	/* 5 */ check(ft_strrchr(s, 'H' + 256) == s);
-	/* 6 */ check(ft_strrchr(s2, 'V') == NULL);
-	/* 7 */ check(ft_strrchr(s2, 0) == s2);
+	/* 6 */ check(ft_strrchr(s, '\0' + 256) == s + 9);
+	/* 7 */ check(ft_strrchr(s2, 'V') == NULL);
+	/* 8 */ check(ft_strrchr(s2, 0) == s2);
 }
